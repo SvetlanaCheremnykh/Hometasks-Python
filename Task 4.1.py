eb9fcb06e48a81ab773a80359481346a1c7b1602
Svetlana Cheremnykh <spmalova@yandex.ru>
2022-12-08 10:56:01 +0300

    Task 4.1 is done

---
+++ Task 4.1.py
@@ -0,0 +1,19 @@
+#Вычислить число Пи c заданной точностью d
+#Пример: 
+#при d = 0.001, π = 3.141
+# при d = 0.0001, π = 3.1415 
+
+def calc_pi(eps=1.0e-5):
+    s=0
+    d=1
+    sgn=1
+    while True:
+        a=4/d
+        s=s+sgn*a
+        if a<eps:
+            return s
+        sgn=-sgn
+        d=d+2
+ 
+ 
+print(calc_pi())
